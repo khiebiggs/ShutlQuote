d243d464359d09ace4afd8f880ab37304b8aafc3
Khie Biggs <khiebigzy@gmail.com>
2020-03-05 13:13:13 +0000

    Update tests to cover all vehicles
    Update tests to account for all vehicle types

--- src/test/java/com/shutl/controller/QuoteControllerFunctionalTest.java
+++ src/test/java/com/shutl/controller/QuoteControllerFunctionalTest.java
@@ -80,4 +80,72 @@ public class QuoteControllerFunctionalTest {
         assertEquals(quote.getVehicle(), Vehicle.bicycle);
         assertEquals(quote.getPrice(), new Long(336));
     }
+
+    @Test
+    public void testVariablePricingByVehicle() throws Exception {
+        Quote bicycleQuoteData = new Quote("SW1A1AA", "EC2A3LT", Vehicle.bicycle);
+        MvcResult result = this.mockMvc.perform(post("/quote")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(bicycleQuoteData)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Quote bicycleQuote = objectMapper.readValue(result.getResponse().getContentAsString(), Quote.class);
+        assertEquals(bicycleQuote.getPickupPostcode(), "SW1A1AA");
+        assertEquals(bicycleQuote.getDeliveryPostcode(), "EC2A3LT");
+        assertEquals(bicycleQuote.getVehicle(), Vehicle.bicycle);
+        assertEquals(bicycleQuote.getPrice(), new Long(348));
+
+        Quote motorbikeQuoteData = new Quote("SW1A1AA", "EC2A3LT", Vehicle.motorbike);
+        result = this.mockMvc.perform(post("/quote")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(motorbikeQuoteData)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Quote motorbikeQuote = objectMapper.readValue(result.getResponse().getContentAsString(), Quote.class);
+        assertEquals(motorbikeQuote.getPickupPostcode(), "SW1A1AA");
+        assertEquals(motorbikeQuote.getDeliveryPostcode(), "EC2A3LT");
+        assertEquals(motorbikeQuote.getVehicle(), Vehicle.motorbike);
+        assertEquals(motorbikeQuote.getPrice(), new Long(363));
+
+        Quote parcelCarQuoteData = new Quote("SW1A1AA", "EC2A3LT", Vehicle.parcel_car);
+        result = this.mockMvc.perform(post("/quote")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(parcelCarQuoteData)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Quote parcelCarQuote = objectMapper.readValue(result.getResponse().getContentAsString(), Quote.class);
+        assertEquals(parcelCarQuote.getPickupPostcode(), "SW1A1AA");
+        assertEquals(parcelCarQuote.getDeliveryPostcode(), "EC2A3LT");
+        assertEquals(parcelCarQuote.getVehicle(), Vehicle.parcel_car);
+        assertEquals(parcelCarQuote.getPrice(), new Long(379));
+
+        Quote smallVanQuoteData = new Quote("SW1A1AA", "EC2A3LT", Vehicle.small_van);
+        result = this.mockMvc.perform(post("/quote")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(smallVanQuoteData)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Quote smallVanQuote = objectMapper.readValue(result.getResponse().getContentAsString(), Quote.class);
+        assertEquals(smallVanQuote.getPickupPostcode(), "SW1A1AA");
+        assertEquals(smallVanQuote.getDeliveryPostcode(), "EC2A3LT");
+        assertEquals(smallVanQuote.getVehicle(), Vehicle.small_van);
+        assertEquals(smallVanQuote.getPrice(), new Long(411));
+
+        Quote largeVanQuoteData = new Quote("SW1A1AA", "EC2A3LT", Vehicle.large_van);
+        result = this.mockMvc.perform(post("/quote")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(largeVanQuoteData)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Quote largeVanQuote = objectMapper.readValue(result.getResponse().getContentAsString(), Quote.class);
+        assertEquals(largeVanQuote.getPickupPostcode(), "SW1A1AA");
+        assertEquals(largeVanQuote.getDeliveryPostcode(), "EC2A3LT");
+        assertEquals(largeVanQuote.getVehicle(), Vehicle.large_van);
+        assertEquals(largeVanQuote.getPrice(), new Long(442));
+    }
 }
