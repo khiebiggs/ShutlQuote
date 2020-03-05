6b958eb7b682c9f5e900eeac9d917cbaff541cd7
Khie Biggs <khiebigzy@gmail.com>
2020-03-05 15:02:06 +0000

    Write basic form
    Write basic form, ensure that the state changes as expected

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import QuoteForm from './QuoteForm'
 
 function App() {
   return (
     <div className="App">
-      
+      <QuoteForm />      
     </div>
   );
 }
--- src/main/frontend/src/QuoteForm.js
+++ src/main/frontend/src/QuoteForm.js
@@ -1,10 +1,56 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 const QuoteForm = () => {
+    const [pickup_postcode, setPickupPostcode] = useState('');
+    const [delivery_postcode, setDeliveryPostcode] = useState('');
+    const [vehicle, setVehicle] = useState('bicycle');
 
     return (
-        <div>
+        <div >
+            <div>
+                <h2>
+                    Get a Quote Instantly
+                </h2>
+                <form>
+                    <label>Pickup Postcode:
+                        <input 
+                            value={pickup_postcode}
+                            onChange={event => setPickupPostcode(event.target.value)}
+                            id = "pickup_postcode"
+                            type = "text"
+                            />
+                    </label>
+                    <br />
+                    <label>Delivery Postcode:
+                        <input 
+                            value={delivery_postcode}
+                            onChange={event => setDeliveryPostcode(event.target.value)}
+                            id = "delivery_postcode"
+                            type = "text"
+                            />
+                    </label>
+                    <br />
+                    <label>Vehicle:
+                        <select id="vehicle"
+                                value={vehicle}
+                                onChange={event => setVehicle(event.target.value)}>
+                            <option value="bicycle">Bicyle</option>
+                            <option value="motorbike">Motorbike</option>
+                            <option value="parcel_car">Parcel Car</option>
+                            <option value="small_van">Small Van</option>
+                            <option value="large_van">Large Van</option>
+                        </select>
+                    </label>
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
 
+
+                pickupPostcode: {pickup_postcode}
+                <br />
+                deliveryPostcode: {delivery_postcode}
+                <br />
+                vehicle: {vehicle}
+            </div>
         </div>
     );
 }
