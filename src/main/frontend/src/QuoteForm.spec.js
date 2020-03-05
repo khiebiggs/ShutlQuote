4cd990c895ca01e5c1034bd9afb2523cfec5c77a
Khie Biggs <khiebigzy@gmail.com>
2020-03-05 15:00:39 +0000

    Write tests to make sure state changes
    As we're using hooks, we need to test this via the side effects of the
    state changing - that is, checking the props of the elements that rely
    on the changed state

--- src/main/frontend/src/QuoteForm.spec.js
+++ src/main/frontend/src/QuoteForm.spec.js
@@ -5,3 +5,29 @@ import QuoteForm from './QuoteForm'
 it('Renders', () => {
     mount(<QuoteForm/>)
 })
+
+it('Updates state', () => {
+    const wrapper = mount(<QuoteForm />)
+
+    wrapper.find('#pickup_postcode').simulate('change', {
+        target: {
+           value: "test1" 
+        }
+    })
+    
+    wrapper.find('#delivery_postcode').simulate('change', {
+        target: {
+           value: "test2" 
+        }
+    })
+
+    wrapper.find('#vehicle').simulate('change', {
+        target: {
+           value: "large_van" 
+        }
+    })
+
+    expect(wrapper.find('#pickup_postcode').prop('value')).toEqual('test1')
+    expect(wrapper.find('#delivery_postcode').prop('value')).toEqual('test2')
+    expect(wrapper.find('#vehicle').prop('value')).toEqual('large_van')
+})
