4bb4afd0a2207f877ae26cceb46b6331dab4d19d
Khie Biggs <khiebigzy@gmail.com>
2020-03-05 13:28:38 +0000

    Write basic test to ensure frontend is served
    Test that spring boot can serve a static frontend, that will be built
    using gradle

---
+++ src/test/java/com/shutl/WebApplicationTest.java
@@ -0,0 +1,35 @@
+package com.shutl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class WebApplicationTest {
+    @Autowired private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    public void shouldServeFrontend() throws Exception {
+        this.mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+}
