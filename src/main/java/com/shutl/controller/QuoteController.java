935e192a4d6c27f41a2d52011e08cd0d8d456f48
Khie Biggs <khiebigzy@gmail.com>
2020-03-05 15:27:28 +0000

    Allow cross origin in quotes controller
    This is needed so that the frontend can communicate with the backend.
    This should actually be set via security policy, or avoiding using a
    webpack proxy, but I'm quite short on time so this will have to do for
    now

--- src/main/java/com/shutl/controller/QuoteController.java
+++ src/main/java/com/shutl/controller/QuoteController.java
@@ -1,13 +1,11 @@
 package com.shutl.controller;
 
 import com.shutl.model.Quote;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@CrossOrigin(origins="*")
 @RestController
 public class QuoteController {
 
